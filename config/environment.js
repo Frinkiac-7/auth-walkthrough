6913060aec56ee8a918690822fd52c4b7c3f3d11
frinkiac-7.net <rob@frinkiac-7.net>
2018-04-12 17:53:19 -0400

    Remove ga-wdi-boston.ember-auth   * Required as part of the deploy guide

--- config/environment.js
+++ config/environment.js
@@ -3,7 +3,7 @@
 module.exports = function (environment) {
   'use strict';
   const ENV = {
-    modulePrefix: 'ga-wdi-boston.ember-auth',
+    modulePrefix: 'auth-walkthrough',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
